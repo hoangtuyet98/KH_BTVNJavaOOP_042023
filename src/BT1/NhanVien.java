d45a2c918dd114e506c2c56c7c394f8c0d37f69b
hoangtuyet98 <hoangtuyet010498@gmail.com>
2023-05-30 06:23:37 +0700

    commit

---
+++ src/BT1/NhanVien.java
@@ -0,0 +1,29 @@
+package BT1;
+
+public class NhanVien {
+    String name="Anh tester";
+    int age=30;
+    String adress="Can tho";
+
+    public NhanVien(String name, int age, String adress) {
+        this.name = name;
+        this.age = age;
+        this.adress = adress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/BT1/ThongTin.java
@@ -0,0 +1,11 @@
+package BT1;
+
+public class ThongTin {
+    public static void main(String[] args) {
+        NhanVien nv=new NhanVien("Tuyet",25,"Ha noi");
+        System.out.println(nv.getName());
+        System.out.println(nv.getAge());
+        System.out.println(nv.getAdress());
+        
+    }
+}
